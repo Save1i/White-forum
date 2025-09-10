1112f4b25a3cbb249c5ec72194d5ddeeca97bf39
Save1i <savva0512@gmail.com>
2025-09-10 17:33:23 +0300

    add: priority route

--- server/src/routes/postRouter.ts
+++ server/src/routes/postRouter.ts
@@ -1,3 +1,4 @@
+import { checkRole } from "@/middleware/checkRole";
 import { Router } from "express";
 import allControllers from "src/controllers/allControllers";
 
@@ -13,5 +14,6 @@ router.post("/:postId/comments", allControllers.addComment);
 
 router.post("/:postId/like", allControllers.toggleLike);
 router.post("/:postId/favorite", allControllers.toggleFavorite);
+router.post("/:postId/priority", checkRole('admin'), allControllers.togglePriority);
 
 export default router;
