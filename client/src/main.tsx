71203cf946afd3161beda69fe0805f6d5c8a8350
Save1i <savva0512@gmail.com>
2025-09-10 17:22:44 +0300

    add: page all users

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,7 +9,8 @@ import App from './App';
 import ErrorPage from './error-page';
 import MessageAndComments from './pages/MessageAndComments';
 import LogIn from './components/LogIn';
-import Profile from './pages/profile';
+import Profile from './pages/Profile';
+import AllUsers from './pages/AllUsers';
 
 
 const router = createBrowserRouter([
@@ -31,6 +32,11 @@ const router = createBrowserRouter([
   {
     path: "/user/:id",
     element: <Profile />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/user",
+    element: <AllUsers />,
     errorElement: <ErrorPage />,
   },
 ]);
